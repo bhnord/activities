8b750a0b720d7a2ea0b11e193c3f2d553288fcc4
bhnord <b.nordemann@aol.com>
2025-02-26 00:12:08 -0500

    get top sites

--- main.py
+++ main.py
@@ -7,8 +7,22 @@ from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 import re
 
-## todo: get dynamically from https://www.thebostoncalendar.com/
+## todo: get dynamically from https://www.thebostoncalendar.com
 url = "https://www.thebostoncalendar.com/events/80-free-things-to-do-in-boston-this-week-feb-18-23-2025"
+
+BASE_URL = "https://www.thebostoncalendar.com"
+
+
+def getEventsLists(n: int):
+    content = requests.get(BASE_URL).text
+    soup = BeautifulSoup(content, "lxml")
+    events = soup.find(id="events")  # ul of events
+    n_events = events.find_all(class_="event")[:n]
+    event_list_links = []
+    for event in n_events:
+        link = str(event.find("a").get("href"))
+        event_list_links.append(link)
+    return event_list_links
 
 
 def getDescriptionText(bitly_link: str) -> str:
@@ -39,17 +53,23 @@ def addToFile(item: Tag, file: TextIOWrapper):
         file.write(event)
 
 
-if __name__ == "__main__":
+def addEvent(url: str, file: TextIOWrapper):
     content = requests.get(url).text
     soup = BeautifulSoup(content, "lxml")
 
     header = soup.find("h1").text.strip()
+
+    items = soup.find(id="event_description")
+    file.write("# " + header + "\n\n")
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(addToFile, items.findChildren(), itertools.repeat(file))
+
+
+if __name__ == "__main__":
     filename = datetime.now().strftime("%m-%d-%Y")
 
-    events = []
-    items = soup.find(id="event_description")
+    event_list_urls = getEventsLists(2)
     with open(filename + ".md", "w") as file:
-        file.write("# " + header + "\n\n")
-
-        with ThreadPoolExecutor(max_workers=4) as executor:
-            executor.map(addToFile, items.findChildren(), itertools.repeat(file))
+        for event_url in event_list_urls:
+            addEvent(BASE_URL + event_url, file)
